68374659bdb959d61863ecaa6a862b28e4ca4c75
sqrteipi <wongcheukiu@gmail.com>
2024-05-06 02:20:42 +0800

    Added Collide System

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame # type: ignore
-from math import sin, cos, pi
+from math import sin, cos, pi, dist
 
 # pygame setup
 pygame.init()
@@ -10,13 +10,12 @@ running = True
 WHITE = (255, 255, 255)
 
 core_ang = 0.0
-core_speed = 3.05
+core_speed = 2.5
 core_radii = 50
 line_len = 300
+snipe_radii = 30
 snipes = []
 pressing = False
-def ang_diff (a, b) :
-    return min(a-b, 360-a+b)
 
 while running:
     for event in pygame.event.get():
@@ -34,22 +33,38 @@ while running:
     if keys[pygame.K_SPACE]:
         if not pressed:
             pressed = True
-            snipes.append(180.0)
+            snipes.append(0.0)
     else:
         pressed = False
 
+    loc = []
     for i in range(len(snipes)):
         angle = snipes[i]
         x = sin(angle*pi/180)*line_len
         y = cos(angle*pi/180)*line_len
         pygame.draw.line(screen, WHITE, [500, 500], [500+x, 500+y], width=5)
-        pygame.draw.circle(screen, WHITE, [500+x, 500+y], radius=30)
+        pygame.draw.circle(screen, WHITE, [500+x, 500+y], radius=snipe_radii)
         snipes[i] += core_speed
         snipes[i] %= 360
-    
+        loc.append([x, y])
+
     pygame.draw.circle(screen, WHITE, [500, 500], core_radii)
 
     pygame.display.flip()
     clock.tick(60)  
+
+    collides = False
+    for i in range(len(loc)):
+        for j in range(i+1, len(loc)):
+            if dist(loc[i], loc[j]) <= 2*snipe_radii:
+                collides = True
+
+    if collides:
+        while True:
+            if not running:
+                break
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
 pygame.quit()
