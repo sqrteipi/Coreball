8a9c445d6cd921487bae1d75f98c9fc2d70271b9
sqrteipi <wongcheukiu@gmail.com>
2024-05-06 03:09:13 +0800

    Restart Function added

--- main.py
+++ main.py
@@ -1,97 +1,107 @@
 import pygame # type: ignore
 from math import sin, cos, pi, dist
-
+import sys
 # pygame setup
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
 SCREEN_MIDDLE = [SCREEN_WIDTH // 2, SCREEN_HEIGHT // 3]
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-running = True
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
 
-WHITE = (255, 255, 255)
+    WHITE = (255, 255, 255)
 
-core_ang = 0.0
-core_speed = 2.5
-core_radii = 50
-line_len = 300
-snipe_radii = 30
-snipes = []
-pressing = False
+    core_ang = 0.0
+    core_speed = 2.5
+    core_radii = 50
+    line_len = 300
+    snipe_radii = 30
+    snipes = []
+    pressing = False
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
 
-    screen.fill("black")
+        screen.fill("black")
 
-    core_ang += core_speed
-    core_ang %= 360
+        core_ang += core_speed
+        core_ang %= 360
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
-        if not pressed:
-            pressed = True
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            if not pressed:
+                pressed = True
 
-            k = SCREEN_MIDDLE[1] + line_len + 300
-            while k > SCREEN_MIDDLE[1] + line_len:
-                k -= 100
-                
-                screen.fill("black")
-                for i in range(len(snipes)):
-                    angle = snipes[i]
-                    x = sin(angle*pi/180)*line_len
-                    y = cos(angle*pi/180)*line_len
-                    pygame.draw.line(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]], [SCREEN_MIDDLE[0]+x, SCREEN_MIDDLE[1]+y], width=5)
-                    pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0]+x, SCREEN_MIDDLE[1]+y], radius=snipe_radii)
+                k = SCREEN_MIDDLE[1] + line_len + 300
+                while k > SCREEN_MIDDLE[1] + line_len:
+                    k -= 100
+                    
+                    screen.fill("black")
+                    for i in range(len(snipes)):
+                        angle = snipes[i]
+                        x = sin(angle*pi/180)*line_len
+                        y = cos(angle*pi/180)*line_len
+                        pygame.draw.line(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]], [SCREEN_MIDDLE[0]+x, SCREEN_MIDDLE[1]+y], width=5)
+                        pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0]+x, SCREEN_MIDDLE[1]+y], radius=snipe_radii)
 
-                pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]], core_radii)
+                    pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]], core_radii)
 
-                pygame.draw.line(screen, WHITE, [SCREEN_MIDDLE[0], k-line_len], [SCREEN_MIDDLE[0], k], width=5)
-                pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], k], snipe_radii)
+                    pygame.draw.line(screen, WHITE, [SCREEN_MIDDLE[0], k-line_len], [SCREEN_MIDDLE[0], k], width=5)
+                    pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], k], snipe_radii)
 
-                pygame.display.flip()
-                clock.tick(60)  
+                    pygame.display.flip()
+                    clock.tick(60)  
 
-            snipes.append(0.0)
-    else:
-        pressed = False
+                snipes.append(0.0)
+        else:
+            pressed = False
+        if keys[pygame.K_r]:
+            main()
 
-    loc = []
-    for i in range(len(snipes)):
-        angle = snipes[i]
-        x = sin(angle*pi/180)*line_len
-        y = cos(angle*pi/180)*line_len
-        pygame.draw.line(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]], [SCREEN_MIDDLE[0]+x, SCREEN_MIDDLE[1]+y], width=5)
-        pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0]+x, SCREEN_MIDDLE[1]+y], radius=snipe_radii)
-        snipes[i] += core_speed
-        snipes[i] %= 360
-        loc.append([x, y])
+        loc = []
+        for i in range(len(snipes)):
+            angle = snipes[i]
+            x = sin(angle*pi/180)*line_len
+            y = cos(angle*pi/180)*line_len
+            pygame.draw.line(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]], [SCREEN_MIDDLE[0]+x, SCREEN_MIDDLE[1]+y], width=5)
+            pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0]+x, SCREEN_MIDDLE[1]+y], radius=snipe_radii)
+            snipes[i] += core_speed
+            snipes[i] %= 360
+            loc.append([x, y])
 
-    pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]], core_radii)
-    
-    pygame.draw.line(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]+300], [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]+300+(line_len-100)], width=5)
-    pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]+300+(line_len-100)], snipe_radii)
+        pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]], core_radii)
+        
+        pygame.draw.line(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]+300], [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]+300+(line_len-100)], width=5)
+        pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]+300+(line_len-100)], snipe_radii)
 
-    pygame.display.flip()
-    clock.tick(60)  
+        pygame.display.flip()
+        clock.tick(60)  
 
-    collides = False
-    for i in range(len(loc)):
-        for j in range(i+1, len(loc)):
-            if dist(loc[i], loc[j]) <= 2*snipe_radii:
-                collides = True
+        collides = False
+        for i in range(len(loc)):
+            for j in range(i+1, len(loc)):
+                if dist(loc[i], loc[j]) <= 2*snipe_radii:
+                    collides = True
 
-    if collides:
-        while True:
-            if not running:
-                break
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+        if collides:
+            while True:
+                if not running:
+                    break
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_r]:
+                    main()
                     running = False
+    pygame.quit()
+    sys.exit()
+    
 
-pygame.quit()
+main()
