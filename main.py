0a56957d6d64308f884ce79022a42e48b534637f
sqrteipi <137189595+sqrteipi@users.noreply.github.com>
2024-05-13 16:59:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import pygame # type: ignore
+import pygame  # type: ignore
 from math import sin, cos, pi, dist
 import sys
+
 # pygame setup
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
-SCREEN_MIDDLE = [SCREEN_WIDTH // 2, SCREEN_HEIGHT // 3]
+SCREEN_MIDDLE = [SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4]
+
 
 def main():
     pygame.init()
@@ -13,6 +15,7 @@ def main():
     running = True
 
     WHITE = (255, 255, 255)
+    font = pygame.font.Font(None, 80)
 
     core_ang = 0.0
     core_speed = 2.5
@@ -20,18 +23,26 @@ def main():
     line_len = 300
     snipe_radii = 30
     snipes = []
-    pressing = False
+    pressed = False
+
+    REVERSED = True
+    SPEED_UP = True
+    SPEED_ADD = True
+
+    round = 0
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-
         screen.fill("black")
 
         core_ang += core_speed
         core_ang %= 360
+
+        text = font.render(f"SCORE: {len(snipes)}", True, WHITE)
+        screen.blit(text, (0, 0))
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE]:
@@ -41,52 +52,107 @@ def main():
                 k = SCREEN_MIDDLE[1] + line_len + 300
                 while k > SCREEN_MIDDLE[1] + line_len:
                     k -= 100
-                    
+
                     screen.fill("black")
                     for i in range(len(snipes)):
                         angle = snipes[i]
-                        x = sin(angle*pi/180)*line_len
-                        y = cos(angle*pi/180)*line_len
-                        pygame.draw.line(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]], [SCREEN_MIDDLE[0]+x, SCREEN_MIDDLE[1]+y], width=5)
-                        pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0]+x, SCREEN_MIDDLE[1]+y], radius=snipe_radii)
+                        x = sin(angle * pi / 180) * line_len
+                        y = cos(angle * pi / 180) * line_len
+                        pygame.draw.line(
+                            screen,
+                            WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]],
+                            [SCREEN_MIDDLE[0] + x, SCREEN_MIDDLE[1] + y],
+                            width=5)
+                        pygame.draw.circle(
+                            screen,
+                            WHITE,
+                            [SCREEN_MIDDLE[0] + x, SCREEN_MIDDLE[1] + y],
+                            radius=snipe_radii)
 
-                    pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]], core_radii)
+                    pygame.draw.circle(screen, WHITE,
+                                       [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]],
+                                       core_radii)
 
-                    pygame.draw.line(screen, WHITE, [SCREEN_MIDDLE[0], k-line_len], [SCREEN_MIDDLE[0], k], width=5)
-                    pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], k], snipe_radii)
+                    pygame.draw.line(screen,
+                                     WHITE, [SCREEN_MIDDLE[0], k - line_len],
+                                     [SCREEN_MIDDLE[0], k],
+                                     width=5)
+                    pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], k],
+                                       snipe_radii)
+
+                    screen.blit(text, (0, 0))
 
                     pygame.display.flip()
-                    clock.tick(60)  
+                    pygame.display.update()
+
+                    clock.tick(60)
 
                 snipes.append(0.0)
+                if REVERSED:
+                    core_speed = -core_speed
         else:
             pressed = False
+
         if keys[pygame.K_r]:
             main()
 
         loc = []
         for i in range(len(snipes)):
             angle = snipes[i]
-            x = sin(angle*pi/180)*line_len
-            y = cos(angle*pi/180)*line_len
-            pygame.draw.line(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]], [SCREEN_MIDDLE[0]+x, SCREEN_MIDDLE[1]+y], width=5)
-            pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0]+x, SCREEN_MIDDLE[1]+y], radius=snipe_radii)
+            x = sin(angle * pi / 180) * line_len
+            y = cos(angle * pi / 180) * line_len
+            pygame.draw.line(screen,
+                             WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]],
+                             [SCREEN_MIDDLE[0] + x, SCREEN_MIDDLE[1] + y],
+                             width=5)
+            pygame.draw.circle(screen,
+                               WHITE,
+                               [SCREEN_MIDDLE[0] + x, SCREEN_MIDDLE[1] + y],
+                               radius=snipe_radii)
             snipes[i] += core_speed
             snipes[i] %= 360
             loc.append([x, y])
 
-        pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]], core_radii)
-        
-        pygame.draw.line(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]+300], [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]+300+(line_len-100)], width=5)
-        pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]+300+(line_len-100)], snipe_radii)
+        pygame.draw.circle(screen, WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1]],
+                           core_radii)
 
+        pygame.draw.line(
+            screen,
+            WHITE, [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1] + 300],
+            [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1] + 300 + (line_len - 180)],
+            width=5)
+        pygame.draw.circle(
+            screen, WHITE,
+            [SCREEN_MIDDLE[0], SCREEN_MIDDLE[1] + 300 + (line_len - 180)],
+            snipe_radii)
+
+        if SPEED_UP:
+            if round % 100 == 1:
+                if core_speed >= 0:
+                    core_speed += 4.5
+                else:
+                    core_speed -= 4.5
+            elif round % 100 == 30:
+                if core_speed >= 0:
+                    core_speed -= 4.5
+                else:
+                    core_speed += 4.5
+
+        if SPEED_ADD:
+            if core_speed >= 0 and core_speed < 10:
+                core_speed += 0.003
+            elif core_speed <= 0 and core_speed < -10:
+                core_speed -= 0.003
+
+        round += 1
         pygame.display.flip()
-        clock.tick(60)  
+        pygame.display.update()
+        clock.tick(60)
 
         collides = False
         for i in range(len(loc)):
-            for j in range(i+1, len(loc)):
-                if dist(loc[i], loc[j]) <= 2*snipe_radii:
+            for j in range(i + 1, len(loc)):
+                if dist(loc[i], loc[j]) <= 2 * snipe_radii:
                     collides = True
 
         if collides:
@@ -102,6 +168,6 @@ def main():
                     running = False
     pygame.quit()
     sys.exit()
-    
+
 
 main()
